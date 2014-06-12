0ee07b9e505d761b80c62ea67782d052566be7ad
jacoboyen <oyenj@central.edu>
2014-06-12 14:11:01 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Central College IT Services Plugin
+ * Plugin URI: https://github.com/CentralCollege/itservices-wordpress-plugin
+ * Description: Added features for the Central College IT Services department website on WordPress
+ * Version: beta
+ * Author: Jordan Bohr '15 | Gavin MacDonald '16 | Jacob Oyen '04 
+ * Author URI: http://www.central.edu
+ */
+?>
---
+++ readme.md
@@ -0,0 +1,4 @@
+Central College IT Services Plugin
+===================
+
+Added features for the Central College IT Services department website on WordPress.

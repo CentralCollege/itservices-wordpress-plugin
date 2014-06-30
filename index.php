548aa8ad3d6f5111ecf935d893eaf99754f830e6
jacoboyen <oyenj@central.edu>
2014-06-30 15:57:14 -0500

    Fixed #3 and added tags and categories for taxonimies

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-
 <?php
 /**
  * Plugin Name: Central College IT Services Plugin
  * Plugin URI: https://github.com/CentralCollege/itservices-wordpress-plugin
  * Description: Added features for the Central College IT Services department website on WordPress
  * Version: beta
- * Author: Jordan Bohr '15 | Gavin MacDonald '16 | Jacob Oyen '04 
+ * Author: Gavin MacDonald '16
  * Author URI: http://www.central.edu
  */
  
@@ -16,35 +15,35 @@ function itservices_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'itservices_styles' );
 
-
-add_action('init', 'it_create_post_type' );
-// Add help desk post type
+// Create Help desk post type
 function it_create_post_type() {
+	$labels = array(
+		'name' => __( 'Help Desk Posts'),
+		'singular_name' => __( 'Help Desk Post')
+	);
+	
 	register_post_type( 'help_desk', 
 		array(
-			'labels' => array(
-					'name'=> _( 'Help Desk Posts'),
-					'singular_name' => _( 'Help Desk Post')
-			),
-		'public' => true,
-		'has_archive' => true,
-		'show_in_menu' => true,
-		'show_in_admin_bar' => true,
-		'menu_position' => 5,
-		'menu_icon' => 'dashicons-editor-help',
-		'taxonomies' => array('categories', 'tags'),
-		'rewrite' => array('slug' => 'help-desk')
+			'labels' => $labels,
+			'public' => true,
+			'has_archive' => true,
+			'show_in_menu' => true,
+			'show_in_admin_bar' => true,
+			'menu_position' => 5,
+			'menu_icon' => 'dashicons-editor-help',
+			'taxonomies' => array('category', 'post_tag'),
+			'rewrite' => array('slug' => 'help-desk')
 		)
 	);
 }
+add_action('init', 'it_create_post_type');
 
 //Flush rewrite rules on plugin initialization
 function it_rewrite_flush(){
 	it_create_post_type();
 	flush_rewrite_rules();
 }
-register_activation_hook( __FILE__, 'it_rewrite_flush' );
-	
+register_activation_hook( __FILE__, 'it_rewrite_flush' );	
 
 // Add unfiltered html for editor
 function add_unfiltered_caps() {
@@ -53,15 +52,12 @@ function add_unfiltered_caps() {
 }
 add_action( 'init', 'add_unfiltered_caps');
 
-
 // Add custom tinyMCE stylesheet
-	function add_it_editor_styles($mce_css) {
-		if ( ! empty( $mce_css ) )
-			$mce_css .= ',';
-		
-		$mce_css .= plugins_url('css/styles.css', __FILE__);
-		return $mce_css;
-	}
-	add_action('mce_css', 'add_it_editor_styles');
+function add_it_editor_styles($mce_css) {
+	if ( ! empty( $mce_css ) )
+		$mce_css .= ',';
 	
+	$mce_css .= plugins_url('css/styles.css', __FILE__);
+	return $mce_css;
+}
 ?>
